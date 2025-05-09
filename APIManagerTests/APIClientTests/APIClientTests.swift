26722c7118f503f00c11a130c3318e314b0c7869
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-05-09 16:01:52 -0400

    added tests

---
+++ APIManagerTests/APIClientTests/APIClientTests.swift
@@ -0,0 +1,100 @@
+// swiftformat:disable all
+// swiftlint:disable all
+@testable import FavoriteMovies
+import XCTest
+
+final class APIClientTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        URLProtocol.registerClass(MockURLProtocol.self)
+    }
+    
+    override func tearDown() {
+        URLProtocol.unregisterClass(MockURLProtocol.self)
+        super.tearDown()
+    }
+    
+    func testFetchReturnsMockedData() async throws {
+        let mockKey: [UInt8] = [0x41, 0x42, 0x43]
+        let mockSalt: [UInt8] = [0x00, 0x00, 0x00]
+        let mockSource = MockAPIKeys(obfuscatedKey: mockKey, salt: mockSalt)
+        let apiManager = MockAPIManager(apiKey: "mock-api-key", keySource: mockSource)
+        
+        // swiftlint:disable:next non_optional_string_data_conversion
+        let mockData = "{\"key\": \"value\"}".data(using: .utf8)
+        MockURLProtocol.mockData = mockData
+        MockURLProtocol.mockResponse = HTTPURLResponse(
+            // swiftlint:disable:next force_unwrapping
+            url: URL(string: "https://api.themoviedb.org/3/movie/popular")!,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+        )
+        
+        let apiClient = APIClient(keyManager: apiManager)
+        let data = try await apiClient.fetch(path: "movie/popular", responseType: [String: String].self)
+        
+        XCTAssertEqual(data, ["key": "value"])
+    }
+    
+    func testFetchThrowsErrorWhenAPIKeyMissing() async throws {
+        let apiManager = MockAPIManager(apiKey: nil, keySource: MockAPIKeys(obfuscatedKey: [], salt: []))
+        let apiClient = APIClient(keyManager: apiManager)
+
+        do {
+            _ = try await apiClient.fetch(path: "movie/popular", responseType: [String: String].self)
+            XCTFail("Expected error, but fetch succeeded")
+        } catch let error as APIKeyError {
+            XCTAssertEqual(error, .missingKey)
+        } 
+    }
+
+    func testFetchThrowsOnHTTPError() async throws {
+        let apiManager = MockAPIManager(apiKey: "mock", keySource: MockAPIKeys(obfuscatedKey: [], salt: []))
+        let config = URLSessionConfiguration.ephemeral
+        config.protocolClasses = [MockURLProtocol.self]
+        let mockSession = URLSession(configuration: config)
+        let apiClient = APIClient(keyManager: apiManager, session: mockSession)
+
+        MockURLProtocol.mockData = nil
+        MockURLProtocol.mockResponse = HTTPURLResponse(
+            url: URL(string: "https://api.themoviedb.org/3/movie/popular")!,
+            statusCode: 404,
+            httpVersion: nil,
+            headerFields: nil
+        )
+
+        do {
+            _ = try await apiClient.fetch(path: "movie/popular", responseType: [String: String].self)
+            XCTFail("Expected error due to 404, but fetch succeeded")
+        } catch let error as APICallError {
+            switch error {
+            case .invalidResponse(let code):
+                XCTAssertEqual(code, 404)
+            }
+        }
+    }
+    
+    func testFetchThrowsOnDecodingError() async {
+        let apiManager = MockAPIManager(apiKey: "mock", keySource: MockAPIKeys(obfuscatedKey: [], salt: []))
+        
+        // swiftlint:disable:next non_optional_string_data_conversion
+        let mockData = "not json".data(using: .utf8)
+        MockURLProtocol.mockData = mockData
+        MockURLProtocol.mockResponse = HTTPURLResponse(
+            url: URL(string: "https://api.themoviedb.org/3/movie/popular")!,
+            statusCode: 200,
+            httpVersion: nil,
+            headerFields: nil
+        )
+
+        let apiClient = APIClient(keyManager: apiManager)
+
+        do {
+            _ = try await apiClient.fetch(path: "movie/popular", responseType: [String: String].self)
+            XCTFail("Expected decoding error")
+        } catch {
+            XCTAssertTrue(error is DecodingError)
+        }
+    }
+}
--- APIManagerTests/APIManagerTests.swift
+++ APIManagerTests/APIManagerTests.swift
@@ -37,6 +37,33 @@ final class APIManagerTests: XCTestCase {
         let mockSource = MockAPIKeys(obfuscatedKey: [], salt: [])
         let apiManager = APIManager(keySource: mockSource)
 
-        XCTAssertEqual(apiManager.apiKey, "")
+        XCTAssertEqual(apiManager.apiKey, nil)
+    }
+
+    func testGetAPIKeyThrowsWhenDecodingFails() {
+        let invalidBytes: [UInt8] = [0xFF, 0xFF, 0xFF]
+        let mockSource = MockAPIKeys(obfuscatedKey: invalidBytes, salt: [0x00, 0x00, 0x00])
+
+        let apiManager = APIManager(keySource: mockSource)
+
+        XCTAssertThrowsError(try apiManager.getAPIKey()) { error in
+            XCTAssertEqual(error as? APIKeyError, .decodingFailed)
+        }
+    }
+
+    func testGetAPIKeySucceedsWithMockedData() {
+        // "ABC" = [0x41, 0x42, 0x43]
+        let mockKey: [UInt8] = [0x41, 0x42, 0x43]
+        let mockSalt: [UInt8] = [0x00, 0x00, 0x00]
+        let mockSource = MockAPIKeys(obfuscatedKey: mockKey, salt: mockSalt)
+
+        let apiManager = APIManager(keySource: mockSource)
+
+        do {
+            let key = try apiManager.getAPIKey()
+            XCTAssertEqual(key, "ABC", "API Key should be correctly decoded")
+        } catch {
+            XCTFail("getAPIKey() should not throw when data is valid")
+        }
     }
 }
