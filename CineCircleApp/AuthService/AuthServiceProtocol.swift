b641966299e53e8fbc9c1c9c9d75a870df27cc1e
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-05-28 11:06:21 -0400

    added doc comments to `AuthServiceProtocol`

--- CineCircleApp/AuthService/AuthServiceProtocol.swift
+++ CineCircleApp/AuthService/AuthServiceProtocol.swift
@@ -1,7 +1,8 @@
 import Foundation
 import FirebaseAuth
 
-protocol AuthServiceProtocol {
+/// Abstracts Firebase authentication methods for dependency injection and testing.
+protocol FirebaseAuthProtocol {
     func createAnAccount(email: String, password: String, completion: @escaping (AuthDataResult?, Error?) -> Void)
     func signIn(email: String, password: String, completion: @escaping (AuthDataResult?, Error?) -> Void)
     func signOut() throws
