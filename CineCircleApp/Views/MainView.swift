9e1c272f49ffb3172d3bd0e5827797dae2841ebe
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-05-29 13:45:59 -0400

    created `MainView` for tabs

---
+++ CineCircleApp/Views/MainView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct MainView: View {
+    @EnvironmentObject var authService: AuthService
+
+    var body: some View {
+        TabView {
+            Tab("Friends", systemImage: "person.and.person") {
+                FilteredFriendList()
+            }
+
+            Tab("Movies", systemImage: "film.stack") {
+                MoviesListView()
+            }
+
+            Tab("Actors", systemImage: "person.crop.square.badge.video.fill") {
+                ActorListView()
+            }
+
+            Tab("Settings", systemImage: "gearshape.fill") {
+                Button("Sign out") {
+                    authService.signOut()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+        .modelContainer(SampleData.shared.modelContainer)
+}
