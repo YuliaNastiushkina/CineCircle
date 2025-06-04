85d539b801ae32aed5f0ea370394726b5638934f
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-06-04 15:43:37 -0400

    created enum of MoviesGenre

---
+++ CineCircleApp/Views/ProfileViews/MoviesGenre.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+enum MoviesGenre: String, CaseIterable {
+    case action
+    case comdey
+    case drama
+    case romance
+    case horror
+    case fantasy
+    case sciFi = "sci-fi"
+    case mystery
+    case thriller
+    case western
+    case adventure
+    case crime
+    case detective
+    case historical
+}
