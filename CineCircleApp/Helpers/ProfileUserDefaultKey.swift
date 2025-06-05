4617df7ba3b64a995e94d4e641a15c535d71e380
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-06-05 12:24:13 -0400

    created `ProfileUserDefaultsKeys`

---
+++ CineCircleApp/Helpers/ProfileUserDefaultKey.swift
@@ -0,0 +1,7 @@
+/// Keys used for storing and retrieving user profile data from UserDefaults.
+enum ProfileUserDefaultsKeys {
+    /// Key for storing the user's name.
+    static let name = "user_name"
+    /// Key for storing the user's favorite movie genres.
+    static let favoriteGenres = "user_favorite_genres"
+}
---
+++ CineCircleApp/ViewModels/ProfileUserDefaultKey.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileUserDefaultKey.swift
+//  CineCircle
+//
+//  Created by Yulya on 2025-06-04.
+//
+
+import Foundation
