5cdd2e09acb8d7aa81dd420d2ec2053ed360af4d
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-09-02 14:27:14 -0400

    updated style of authentication view

--- CineCircleApp/Views/StartViews/AuthenticationView.swift
+++ CineCircleApp/Views/StartViews/AuthenticationView.swift
@@ -3,27 +3,64 @@ import SwiftUI
 struct AuthenticationView: View {
     var body: some View {
         VStack {
-            AuthenticationFieldsView(email: $viewModel.email, password: $viewModel.password)
-
-            if !viewModel.errorMessage.isEmpty {
-                Text(viewModel.errorMessage)
-                    .foregroundColor(.red)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal)
+            HStack {
+                Spacer()
+                Button {
+                    isSigningUp.toggle()
+                    viewModel.email = ""
+                    viewModel.password = ""
+                } label: {
+                    signUpButton
+                }
             }
+            .padding(.horizontal, padding16)
+            .padding(.top, topPadding)
 
-            Button("\(isSigningUp ? "Sign up" : "Sign in")") {
-                authenticate()
-            }
-            .buttonStyle(.borderedProminent)
-            .controlSize(.large)
+            Spacer()
 
-            Button("\(isSigningUp ? "I already have an account" : "I don't have an account")") {
-                isSigningUp.toggle()
+            VStack(alignment: .leading, spacing: spacing) {
+                Text(isSigningUp ? signUpWelcomeText : logInWilcomeText)
+                    .font(Font.custom(poppinsBoldFont, size: welcomeTextFontSize))
+                    .foregroundStyle(Color.black)
+                    .frame(width: frameForWelcomeText, alignment: .topLeading)
+
+                if isSigningUp {
+                    Text(signUpDescription)
+                        .font(Font.custom(poppinsFont, size: smallTextSize))
+                        .foregroundStyle(Color.black)
+                        .frame(width: frameForWelcomeText, alignment: .topLeading)
+                }
+
+                AuthenticationFieldsView(
+                    email: $viewModel.email,
+                    password: $viewModel.password
+                )
+
+                if !viewModel.errorMessage.isEmpty {
+                    Text(viewModel.errorMessage)
+                        .font(Font.custom(poppinsFont, size: authenticationAlertSize))
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal)
+                }
+
+                Button {
+                    authenticate()
+                } label: {
+                    logInButton
+                }
+                .padding(.vertical, padding16)
+                .frame(width: frameForLogInButton, alignment: .center)
+                .background(buttonColor)
+                .cornerRadius(buttonCornerRadius)
+
+                Divider()
             }
-            .padding(.top)
+            .padding(.horizontal, padding16)
+
+            Spacer()
         }
-        .padding()
+        .background(Color.white.ignoresSafeArea())
     }
 
     // MARK: Private interface
@@ -46,6 +83,38 @@ struct AuthenticationView: View {
     private func authenticate() {
         isSigningUp ? signUp() : signIn()
     }
+
+    private var logInButton: some View {
+        Text(isSigningUp ? "Sign up" : "Log in")
+            .font(Font.custom(poppinsSemiBoldFont, size: logInButtonTextSize))
+            .foregroundColor(Color.black)
+    }
+
+    private var signUpButton: some View {
+        Text(isSigningUp ? "Log in" : "Sign Up")
+            .font(Font.custom(poppinsFont, size: signUpTextSize))
+            .foregroundColor(Color.black)
+    }
+
+    private let signUpWelcomeText = "Hello there 👋"
+    private let signUpDescription = "Sign up and dive into a world of cinema."
+    private let logInWilcomeText = "Welcome back\nto CineCircle!"
+    private let poppinsFont = "Poppins"
+    private let poppinsBoldFont = "Poppins-Bold"
+    private let poppinsSemiBoldFont = "Poppins-SemiBold"
+    private let welcomeTextFontSize: CGFloat = 36
+    private let smallTextSize: CGFloat = 14
+    private let signUpTextSize: CGFloat = 16
+    private let authenticationAlertSize: CGFloat = 14
+    private let frameForWelcomeText: CGFloat = 280
+    private let logInButtonTextSize: CGFloat = 16
+    private let frameForLogInButton: CGFloat = 370
+    private let buttonColor: Color = .init(red: 1, green: 0.83, blue: 0.24)
+    private let buttonCornerRadius: CGFloat = 24
+    private let spacing: CGFloat = 24
+    private let padding16: CGFloat = 16
+    private let topPadding: CGFloat = 8
+    private let lineWidth: CGFloat = 1
 }
 
 #Preview {
