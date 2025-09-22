e0585b718c118e1561ad970b5be962e4247fd092
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-09-22 12:33:16 -0400

    created `DetailedInfoPresentation`

---
+++ CineCircleApp/Models/Movie/Crew/DetailedInfoPresentation.swift
@@ -0,0 +1,12 @@
+/// Aggregated presentation of detailed movie information.
+/// Values are already formatted for direct use in UI.
+struct DetailedInfoPresentation {
+    let directors: String
+    let producers: String
+    let screenwriters: String
+    let productionCompanies: String
+    let genres: String
+    let originalLanguage: String
+    let releaseDate: String
+    let runtime: String
+}
