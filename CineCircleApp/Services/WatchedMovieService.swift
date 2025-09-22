e49b2585a34681046fe999db6a26091e7ba1471e
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2025-09-22 14:43:16 -0400

    updated `WatchedMovieService` and entity

--- CineCircleApp/Services/WatchedMovieService.swift
+++ CineCircleApp/Services/WatchedMovieService.swift
@@ -3,53 +3,23 @@ import Foundation
 
 /// A service for managing the user's watched movies using Core Data.
 final class WatchedMovieService {
-    // MARK: Private interface
+    // MARK: - Private interface
 
-    private let context: NSManagedObjectContext
+    private let impl: MovieFlagService<WatchedMovie>
 
-    // MARK: Internal interface
+    // MARK: - Internal interface
 
-    /// The shared singleton instance of `WatchedMovieService`.
     static let shared = WatchedMovieService(context: CoreDataManager.shared.context)
 
-    ///  Initializes a new instance of `WatchedMovieService`.
-    /// - Parameter context: The Core Data context to use for operations.
     init(context: NSManagedObjectContext) {
-        self.context = context
+        impl = MovieFlagService<WatchedMovie>(context: context)
     }
 
-    /// Checks whether the specified movie is marked as watched by the user.
-    /// - Parameters:
-    ///   - movieId: The ID of the movie.
-    ///   - userId: The ID of the user.
-    /// - Returns: `true` if the movie is marked as watched; otherwise, `false`.
     func isWatched(movieId: Int, userId: String) -> Bool {
-        let request = WatchedMovie.fetchRequest()
-        request.predicate = NSPredicate(format: "movieID == %d AND userID == %@", movieId, userId)
-        return (try? context.fetch(request).first) != nil
+        impl.isSet(movieId: movieId, userId: userId)
     }
 
-    /// Toggles the watched status of a movie for the user.
-    ///
-    /// If the movie is already marked as watched, the existing record will be removed.
-    /// Otherwise, a new record will be created.
-    /// - Parameters:
-    ///   - movieId: The ID of the movie.
-    ///   - userId: The ID of the user.
     func toggleWatched(movieId: Int, userId: String) {
-        let request = WatchedMovie.fetchRequest()
-        request.predicate = NSPredicate(format: "movieID == %d AND userID == %@", movieId, userId)
-
-        if let existing = try? context.fetch(request).first {
-            context.delete(existing)
-        } else {
-            let new = WatchedMovie(context: context)
-            new.id = UUID()
-            new.movieID = Int32(movieId)
-            new.userID = userId
-            new.createdAt = Date()
-        }
-
-        try? context.save()
+        impl.toggle(movieId: movieId, userId: userId)
     }
 }
