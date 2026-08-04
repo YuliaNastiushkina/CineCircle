707974f640e64ccfde90f4ea342b64c27e06a172
Yulya Nastiushkina <y.nastiushkina@gmail.com>
2026-08-04 12:38:49 -0400

    Configure Firebase App Check for debug builds

--- CineCircleApp/App/AppDelegate.swift
+++ CineCircleApp/App/AppDelegate.swift
@@ -1,9 +1,16 @@
+import FirebaseAppCheck
 import FirebaseCore
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
     func application(_: UIApplication,
                      didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        #if DEBUG
+            AppCheck.setAppCheckProviderFactory(
+                AppCheckDebugProviderFactory()
+            )
+        #endif
+
         FirebaseApp.configure()
         return true
     }
